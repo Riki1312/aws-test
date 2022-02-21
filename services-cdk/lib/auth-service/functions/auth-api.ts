e34c9271ed3221d52fa2f90140f1505cc904c6fc
Riccardo <riki.brero@gmail.com>
2022-02-21 02:17:50 +0100

    Update auth-api.ts

--- services-cdk/lib/auth-service/functions/auth-api.ts
+++ services-cdk/lib/auth-service/functions/auth-api.ts
@@ -17,52 +17,55 @@ export const handler = (event: APIGatewayProxyEventV2, context: Context, callbac
     else callback("Email required");
   }
 
-  // Check if the user exists and create it if not.
   identityService.listUsers(
     {
       UserPoolId: env.USER_POOL_ID!,
-      Filter: `email=${email}`,
+      Filter: `email="${email}"`,
       Limit: 1,
     },
     (error, data) => {
       if (error) {
         callback(error);
-      } else if (data.Users && data.Users.length == 0) {
+      } else {
         // User not exist: sing up.
-        identityService.signUp(
-          {
-            ClientId: env.CLIENT_ID!,
-            Username: email,
-            Password: randomBytes(64).toString("hex"),
-            UserAttributes: [
-              {
-                Name: "email",
-                Value: email,
+        if (data.Users && data.Users.length == 0) {
+          identityService.signUp(
+            {
+              ClientId: env.CLIENT_ID!,
+              Username: email,
+              Password: randomBytes(64).toString("hex"),
+              UserAttributes: [
+                {
+                  Name: "email",
+                  Value: email,
+                },
+              ],
+            },
+            (error, data) => {
+              if (error) callback(error);
+              else callback(null, data);
+            }
+          );
+        } else {
+          // User already exists: initiate auth.
+          identityService.adminInitiateAuth(
+            {
+              UserPoolId: env.USER_POOL_ID!,
+              ClientId: env.CLIENT_ID!,
+              AuthFlow: "CUSTOM_AUTH",
+              AuthParameters: {
+                USERNAME: email,
               },
-            ],
-          },
-          (error, data) => {
-            if (error) callback(error);
-            else callback(null, data);
-          }
-        );
+            },
+            (error, data) => {
+              if (error) callback(error);
+              else callback(null, data);
+            }
+          );
+        }
       }
     }
   );
 
-  // The user exists, proceed with the login.
-  identityService.adminInitiateAuth(
-    {
-      UserPoolId: env.USER_POOL_ID!,
-      ClientId: env.CLIENT_ID!,
-      AuthFlow: "CUSTOM_AUTH",
-      AuthParameters: {
-        USERNAME: email,
-      },
-    },
-    (error, data) => {
-      if (error) callback(error);
-      else callback(null, data);
-    }
-  );
+  //
 };
