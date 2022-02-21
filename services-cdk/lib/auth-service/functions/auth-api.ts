3f5d7760608f0db80835cfc9dc24c2e8fc891d48
Riccardo <riki.brero@gmail.com>
2022-02-21 02:34:11 +0100

    Update auth-api.ts

--- services-cdk/lib/auth-service/functions/auth-api.ts
+++ services-cdk/lib/auth-service/functions/auth-api.ts
@@ -1,11 +1,11 @@
-import { APIGatewayProxyEventV2, Callback, Context } from "aws-lambda";
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2, Context } from "aws-lambda";
 import { CognitoIdentityServiceProvider } from "aws-sdk";
 import { randomBytes } from "crypto";
 import { env } from "process";
 
 const identityService = new CognitoIdentityServiceProvider();
 
-export const handler = (event: APIGatewayProxyEventV2, context: Context, callback: Callback) => {
+export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
   console.log("Event:", JSON.stringify(event, null, 2));
 
   let email = "";
@@ -14,58 +14,59 @@ export const handler = (event: APIGatewayProxyEventV2, context: Context, callbac
     let body = JSON.parse(event.body);
 
     if (body.email) email = body.email;
-    else callback("Email required");
+    else {
+      return {
+        statusCode: 400,
+        body: "Email required",
+      };
+    }
   }
 
-  identityService.listUsers(
-    {
+  // Check if the user exists.
+  const listUsers = await identityService
+    .listUsers({
       UserPoolId: env.USER_POOL_ID!,
       Filter: `email="${email}"`,
       Limit: 1,
-    },
-    (error, data) => {
-      if (error) {
-        callback(error);
-      } else {
-        // User not exist: sing up.
-        if (data.Users && data.Users.length == 0) {
-          identityService.signUp(
-            {
-              ClientId: env.CLIENT_ID!,
-              Username: email,
-              Password: randomBytes(64).toString("hex"),
-              UserAttributes: [
-                {
-                  Name: "email",
-                  Value: email,
-                },
-              ],
-            },
-            (error, data) => {
-              if (error) callback(error);
-              else callback(null, data);
-            }
-          );
-        } else {
-          // User already exists: initiate auth.
-          identityService.adminInitiateAuth(
-            {
-              UserPoolId: env.USER_POOL_ID!,
-              ClientId: env.CLIENT_ID!,
-              AuthFlow: "CUSTOM_AUTH",
-              AuthParameters: {
-                USERNAME: email,
-              },
-            },
-            (error, data) => {
-              if (error) callback(error);
-              else callback(null, data);
-            }
-          );
-        }
-      }
-    }
-  );
+    })
+    .promise();
 
-  //
+  if (listUsers.Users && listUsers.Users.length == 0) {
+    // User not exist: sing up.
+    await identityService
+      .signUp({
+        ClientId: env.CLIENT_ID!,
+        Username: email,
+        Password: randomBytes(64).toString("hex"),
+        UserAttributes: [
+          {
+            Name: "email",
+            Value: email,
+          },
+        ],
+      })
+      .promise();
+  } else if (listUsers.Users && listUsers.Users.length > 0) {
+    // User already exists: initiate auth.
+    const authResponse = await identityService
+      .adminInitiateAuth({
+        UserPoolId: env.USER_POOL_ID!,
+        ClientId: env.CLIENT_ID!,
+        AuthFlow: "CUSTOM_AUTH",
+        AuthParameters: {
+          USERNAME: email,
+        },
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(authResponse),
+    };
+  }
+
+  return {
+    statusCode: 500,
+    body: "Unknown error",
+  };
 };
